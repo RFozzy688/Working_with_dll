855be8ffef9a062fd95b7644a940c4564f6c45db
RFozzy688 <pokerfozzy@gmail.com>
2023-11-11 16:06:00 +0200

    Calc Square

--- Working_with_dll/Program.cs
+++ Working_with_dll/Program.cs
@@ -11,6 +11,8 @@ namespace Working_with_dll
             int c = 20;
 
             Console.WriteLine($"Площадь квадрата: {CalcSquare.Square(a)}");
+            Console.WriteLine($"Площадь прямоугольника: {CalcSquare.Square(a, b)}");
+            Console.WriteLine("Площадь треугольника: {0:f2}", CalcSquare.Square(a, b, c));
         }
     }
 }
